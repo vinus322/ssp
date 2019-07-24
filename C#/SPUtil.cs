89aebce0023eac7ccb8283635fed6487c8c471b7
fact2020 <52699212+fact2020@users.noreply.github.com>
2019-07-24 22:40:48 +0900

    Update SPUtil.cs

--- C#/SPUtil.cs
+++ C#/SPUtil.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -8,18 +9,23 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
- 
+
 namespace SPUtil
 {
     public class SPUtil
     {
-        public SPUtil()
+        static ConcurrentDictionary<string, List<string>> inputData = null;
+        
+        static SPUtil()
         {
- 
+            inputData = new ConcurrentDictionary<string, List<string>>();
+            inputData.TryAdd("A", new List<string>());
+            inputData.TryAdd("B", new List<string>());
+            inputData.TryAdd("C", new List<string>());
         }
- 
+
         /// <summary>
-        /// 입력파일 읽기
+        /// 입력 파일 읽기
         /// </summary>
         /// <param name="fileName"></param>
         public static void ReadFile(string fileName)
@@ -30,12 +36,15 @@ namespace SPUtil
                 while (sr.Peek() > -1)
                 {
                     line = sr.ReadLine();
- 
-                    DoLineJob(line);
+
+                    if (line.Equals("PRINT"))
+                        break;
+
+                    DoFileLineJob(line);
                 }
-            }        
+            }
         }
- 
+
         /// <summary>
         /// 파일 출력
         /// </summary>
@@ -48,9 +57,9 @@ namespace SPUtil
                 outputFile.Write(text); ;
             }
         }
- 
+
         /// <summary>
-        /// 외부 프로그램 실행 기술
+        /// 외부 프로그램 실행
         /// </summary>
         /// <param name="messageCode"></param>
         /// <returns></returns>
@@ -63,7 +72,7 @@ namespace SPUtil
             start.WindowStyle = ProcessWindowStyle.Hidden;
             start.CreateNoWindow = true;
             start.Arguments = args;
- 
+
             using (Process process = Process.Start(start))
             {
                 using (StreamReader reader = process.StandardOutput)
@@ -72,7 +81,7 @@ namespace SPUtil
                 }
             }
         }
- 
+
         /// <summary>
         /// 서버 소켓 시작
         /// </summary>
@@ -81,20 +90,21 @@ namespace SPUtil
             IPEndPoint ep = new IPEndPoint(IPAddress.Any, portNumber);
             TcpListener server = new TcpListener(ep);
             server.Start();
- 
+
             Console.WriteLine("The server socket is started at {0}", ep.ToString());
- 
+
             while (true)
             {
                 TcpClient client = server.AcceptTcpClient();
                 Console.WriteLine("A new client is connected from {0}", ep.ToString());
- 
+
                 ParameterizedThreadStart sTh = new ParameterizedThreadStart(ThreadFunc);
                 Thread th = new Thread(sTh);
                 th.Start(client);
             }
         }
- 
+
+
         /// <summary>
         /// 접속된 클라이언트별 전용 쓰레드
         /// </summary>
@@ -102,23 +112,111 @@ namespace SPUtil
         static void ThreadFunc(object client)
         {
             TcpClient c = (TcpClient)client;
-            NetworkStream st = c.GetStream();
- 
+            NetworkStream ns = c.GetStream();
+
+            byte[] buffer;
+
+            // 1회 전송 데이터의 최대 길이
+            int bufferSize = 1024;
+
+            // 1회 데이터를 읽은 길이
+            int nRead = 0;
+
             string line;
-            using (StreamReader sr = new StreamReader(st))
-            {
-                while (sr.Peek() > -1 && c.Connected )
+            string clientName = "";
+
+            while (c.Connected)
+            { 
+                buffer = new byte[bufferSize];
+
+                try
                 {
-                    line = sr.ReadLine();
- 
-                    DoLineJob(line);
+                    nRead = ns.Read(buffer, 0, bufferSize);
+                    line = Encoding.Default.GetString(buffer, 0, nRead);
+                    
+                    Console.WriteLine("Received Data : {0} from {1}", c.Client.RemoteEndPoint);
+
+                    /* 출력용 클라이언트인가 프린트를 전송 */
+                    if (clientName.Equals("PRINT_CLIENT") && 
+                        line.Equals("PRINT"))
+                    {
+                        string outputData = MakeSocketPrintData();
+
+                        SendData(ns, outputData);
+                        break;
+                    }
+                    else if (line.Equals("CLIENT_NAME"))
+                    {
+                        clientName = line;
+                        inputData[clientName] = new List<string>();
+                    }
+                    else 
+                    {
+                        DoSocketLineJob(clientName, line);
+                    }
+                }
+                catch(Exception ex)
+                {
+                
                 }
             }
         }
- 
-        static void DoLineJob(string line)
+
+        /// <summary>
+        /// 출력 클라이언트에 보내줄 데이터 생성
+        /// </summary>
+        /// <returns></returns>
+        static string MakeSocketPrintData()
+        {
+            string ret = string.Empty;
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 파일인 경우 라인 단위 작업
+        /// </summary>
+        /// <param name="line"></param>
+        static List<string> DoFileLineJob(string line)
         {
             string[] items = line.Split((",").ToCharArray());
+            
+            inputData["A"].Add(items[0]);
+            //inputData["A"].Sort();
+            
+            inputData["B"].Add(items[1]);
+            // inputData["B"].Sort();
+
+            inputData["C"].Add(items[2]);            
+            // inputData["C"].Sort();
+
+            
+            return items.ToList<string>();
+        }
+
+        /// <summary>
+        /// 소켓인 경우 라인 단위 작업
+        /// </summary>
+        /// <param name="line"></param>
+        static void DoSocketLineJob(string clientName, string line)
+        {
+            string[] items = line.Split((",").ToCharArray());
+
+            // 라인을 분석한 후 클라이언트 이름의 자료에 저장
+
+            inputData[clientName].Add(line);
+
+        }
+
+        /// <summary>
+        /// 텍스트 전송
+        /// </summary>
+        /// <param name="ns"></param>
+        /// <param name="data"></param>
+        static void SendData(NetworkStream ns, string data)
+        {
+            byte[] bufs = Encoding.Default.GetBytes(data);
+            ns.Write(bufs, 0, bufs.Length );
         }
     }
 }
